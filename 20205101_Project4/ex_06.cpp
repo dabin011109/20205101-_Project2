7f985cff28a5a524d19cf39c1462750d6db15123
dabin <dabin@DESKTOP-AKF7T84>
2022-11-27 23:34:01 +0900

    내용 추가

--- 20205101_Project4/ex_06.cpp
+++ 20205101_Project4/ex_06.cpp
@@ -1,1 +1,33 @@
 // p514, 2번 문제
+
+#include<iostream>
+using namespace std;
+
+class Weapon 
+{
+public:
+    virtual void load() = 0;
+};
+class Bomb : public Weapon 
+{
+public:
+    void load() { cout << "폭탄을 적재합니다." << endl; }
+};
+class Gun : public Weapon 
+{
+public:
+    void load() { cout << "총을 적재합니다." << endl; }
+};
+
+int main()
+{
+    Weapon* loa[3];
+    loa[0] = new Gun;
+    loa[1] = new Bomb;
+    loa[2] = new Bomb;
+
+    for (Weapon* w : loa)
+        w->load();
+
+    return 0;
+}
--- 20205101_Project4/ex_07.cpp
+++ 20205101_Project4/ex_07.cpp
@@ -132,11 +132,30 @@ int main()
 				throw direction;
 		}
 		catch (char e) {
-			cout << "오류 : " << e << "는 존재하지 않습니다." << endl;
+			cout << "오류 : " << e << " 문자는 존재하지 않습니다." << endl;
 		}
 	}
 
 	for (auto& e : list)
 		delete e;
 	list.clear();
+}// p591, 4번 문제
+
+#include<iostream>
+using namespace std;
+
+template<typename T>
+T getSmalleset(T arr[], int n) {
+	T min = arr[0];
+	for (int i = 0; i < n; i++) {
+		if (min > arr[i])
+			min = arr[i];
+	}
+	return min;
+}
+
+int main()
+{
+	double list[] = { 1.2, 3.3, 9.0, 1.5, 8.7 };
+	cout << "최소값: " << getSmalleset(list, 5) << endl;
 }
